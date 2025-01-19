161748e29d85a5276defa02c73c73784b7736bac
Bharghavi-2006 <dvbiitmadras@gmail.com>
2025-01-19 20:12:40 +0530

    Initial commit

---
+++ controllers/controller.js
@@ -0,0 +1,79 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to the database
+mongoose.connect('mongodb+srv://bhags2006:bhagi2006@birthdaycluster.oilpf.mongodb.net/');
+
+//Create a Schema
+var BirthdaySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true 
+    },
+    birthday: {
+        type: Date,
+        required: true }
+});
+
+var BirthdayModel = mongoose.model('BirthdayModel', BirthdaySchema);
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+module.exports = function (app) {
+
+app.get('/birthday', function(req, res) {
+    //get data from mongodb and pass it to the view
+    BirthdayModel.find({}).then((data) => {
+        res.render('index', { birthdays: data});
+    });
+    
+});
+
+app.post('/birthday', urlencodedParser, function (req, res) {
+    //get data from the view and add it to mongodb
+    var newBirthdayModel = BirthdayModel(req.body);
+    newBirthdayModel.save().then((data) => {
+        res.json(data);
+    });
+});
+
+app.delete('/birthday/:name', function (req, res) {
+    //delete the requested item from mongodb
+    BirthdayModel.findOneAndDelete({ name: req.params.name }).then((deleted) => {
+            if (!deleted) {
+                res.status(404).send('Birthday not found.');                
+            } else {
+                res.json(deleted);
+            }
+        });
+});
+
+//Update a birthday by name 
+app.put('/birthday/:name', urlencodedParser, function (req, res){
+    BirthdayModel.findOneAndUpdate(
+        { name: req.params.name },
+        { $set: { birthday: req.body.birthday } },
+        { new: true },
+    ).then((updated) => {
+            if (!updated) {
+                res.status(404).send('Birthday not found.');
+            } else {
+                res.json(updated);
+            }
+        });
+});
+
+//Search for a birthday by name
+app.get('/birthday/:name', function (req, res) {
+    BirthdayModel.findOne({ name: req.params.name }).then((birthday) => {
+            if (!birthday) {
+                res.status(404).send('Birthday not found.');
+            } else {
+                res.json(birthday);
+            }
+        });
+
+});
+
+};
---
+++ public/assets/index.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+
+    //Add a new birthday
+    $('#birthdayForm').on('submit', function (event) {
+        event.preventDefault();
+        var name = $('#name').val();
+        var birthday = $('#birthday').val();
+        
+        if (!name || !birthday) {
+            alert('Please provide both a name and a valid birthday.');
+            return;
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/birthday',
+            data: { name: name, birthday: birthday },
+            success: function () {
+                //do something with the data via front-end
+                location.reload();
+            },
+            error: function (err) {
+                alert('Error adding birthday: ' + err.responseText);
+            }
+        });
+    });
+    
+    //Delete a birthday
+    $('#birthdayList').on('click', '.delete-button', function () {
+            var name = $(this).closest('li').data('name');
+            $.ajax({
+                type: 'DELETE',
+                url: '/birthday/' + name,
+                success: function () {
+                    //do something with frontend framework
+                    location.reload();
+                },
+                error: function (err) {
+                    alert('Error deleting birthday: ' +err.responseText);
+                }
+            });
+    });
+
+    //Update a birthday
+    $('#birthdayList').on('click', '.update-button', function () {
+        var name = $(this).closest('li').data('name');
+        var newDate = prompt('Enter new birthday (YYYY-MM-DD):');
+
+        if (newDate) {
+            $.ajax({
+                type: 'PUT',
+                url: '/birthday/' + name,
+                data: { birthday: newDate },
+                success: function () {
+                    location.reload();
+                },
+                error: function (err) {
+                    alert('Error updating birthday: ' + err.responseText);
+                }
+            });
+        }
+    });
+
+    //Search for a birthday
+    $('#searchForm').on('submit', function (e) {
+        e.preventDefault();
+        var name = $('#searchName').val().trim();
+
+        if (!name) {
+            alert ('Please enter a name to search.');
+            return;
+        }
+
+        $.ajax({
+            type: 'GET',
+            url: '/birthday/' + name,
+            success: function (data) {
+                if (data) {
+                    $('#searchResult').text(`${data.name}'s birthday is on ${new Date(data.birthday).toDateString()}`);
+                } else {
+                    $('#searchResult').text('No birthday found for that name.');
+                }
+            },
+            error: function (err) {
+                alert('Error fetching birthday: ' + err.responseText);
+            }
+        });
+    });
+});
